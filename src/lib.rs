308a2f1be0006e9524f15eeff4d7bc77fa5b72ab
trxe <peixianwong@gmail.com>
2025-05-02 08:08:59 +0800

    fully setup the single threaded cursor

--- src/lib.rs
+++ src/lib.rs
@@ -3,20 +3,26 @@ pub mod common;
 pub mod from;
 use std::{error::Error, ops::Deref, sync::Arc};
 
+use bson::doc;
 use buffer::{init_buffers, parse_lines};
 use common::{BsonDoc, SyncCursor};
-use mongodb::Cursor;
+use from::Wrap;
+use mongodb::{Cursor, action::Find, sync::Collection};
 use polars::{
     error::{ErrString, PolarsError, PolarsResult},
-    prelude::{AnonymousScan, Column, DataFrame, DataType, LazyFrame, Schema},
+    frame::row::infer_schema,
+    prelude::{
+        AnonymousScan, Column, DataFrame, DataType, DateType, LazyFrame, ScanArgsAnonymous, Schema,
+        SchemaRef,
+    },
 };
 use polars_core::POOL;
 
 pub struct BsonScan {
-    pub cursor: Option<SyncCursor>,
+    pub collection: Collection<BsonDoc>,
+    pub find_doc: Option<BsonDoc>,
     pub infer_schema_length: Option<usize>,
     pub n_threads: Option<usize>,
-    pub rows_per_thread: usize,
     pub ignore_errors: Option<bool>,
     pub needs_escaping: Option<bool>,
     pub allow_null: Option<bool>,
@@ -33,13 +39,60 @@ impl From<MongoError> for std::io::Error {
 }
 
 impl BsonScan {
+    fn new(
+        collection: Collection<BsonDoc>,
+        find_doc: Option<BsonDoc>,
+        options: &BsonScanOptions,
+    ) -> Self {
+        Self {
+            collection,
+            find_doc,
+            infer_schema_length: options.infer_schema_length.clone(),
+            n_threads: None,
+            ignore_errors: Some(false),
+            needs_escaping: Some(false),
+            allow_null: Some(true),
+        }
+    }
+    fn get_cursor(&self) -> PolarsResult<SyncCursor> {
+        let find_doc = self.find_doc.clone().unwrap_or(doc! {});
+        let find: Find<BsonDoc> = self.collection.find(find_doc);
+        let cursor = match find.run() {
+            Ok(x) => x,
+            Err(e) => {
+                return Err(PolarsError::IO {
+                    error: Arc::new(MongoError(e).into()),
+                    msg: Some(ErrString::new_static("Connection with mongodb interrupted")),
+                });
+            }
+        };
+        Ok(cursor)
+    }
+    fn schema_single_threaded_stream(
+        &self,
+        cursor: SyncCursor,
+        infer_schema_length: Option<usize>,
+    ) -> PolarsResult<Schema> {
+        let iter = cursor.map(|doc| {
+            let val = doc.unwrap();
+            val.into_iter()
+                .map(|(key, value)| {
+                    let dtype = Wrap::<DataType>::from(&value);
+                    (key, dtype.0)
+                })
+                .collect()
+        });
+        let schema = infer_schema(iter, infer_schema_length.unwrap_or(DEFAULT_CHUNK_SIZE));
+        Ok(schema)
+    }
     fn scan_single_threaded_stream(
         &self,
         cursor: SyncCursor,
         schema: Arc<Schema>,
+        rows_per_thread: usize,
     ) -> PolarsResult<DataFrame> {
         let mut buffers = init_buffers(
-            self.rows_per_thread,
+            rows_per_thread,
             schema.as_ref(),
             Some('\'' as u8),
             polars::prelude::CsvEncoding::Utf8,
@@ -69,13 +122,65 @@ impl BsonScan {
     }
 }
 
+impl AnonymousScan for BsonScan {
+    fn allows_predicate_pushdown(&self) -> bool {
+        true
+    }
+    fn allows_projection_pushdown(&self) -> bool {
+        true
+    }
+    fn allows_slice_pushdown(&self) -> bool {
+        true
+    }
+    fn scan(&self, scan_opts: polars::prelude::AnonymousScanArgs) -> PolarsResult<DataFrame> {
+        let schema = scan_opts.schema;
+        let doc_cursor = self.get_cursor()?;
+        self.scan_single_threaded_stream(doc_cursor, schema.clone(), DEFAULT_CHUNK_SIZE)
+    }
+    fn schema(
+        &self,
+        _infer_schema_length: Option<usize>,
+    ) -> PolarsResult<polars::prelude::SchemaRef> {
+        let schema_cursor = self.get_cursor()?;
+        let schema = self.schema_single_threaded_stream(schema_cursor, self.infer_schema_length)?;
+        Ok(SchemaRef::new(schema))
+    }
+
+    fn next_batch(
+        &self,
+        _scan_opts: polars::prelude::AnonymousScanArgs,
+    ) -> PolarsResult<Option<DataFrame>> {
+        Ok(None)
+    }
+
+    fn as_any(&self) -> &(dyn std::any::Any + 'static) {
+        todo!()
+    }
+}
+
 #[derive(Debug)]
 pub struct BsonScanOptions {
     pub infer_schema_length: Option<usize>,
+    pub n_rows: Option<usize>,
 }
 
-// pub trait BsonLazyReader {
-//     fn scan_bson(options: BsonScanOptions) -> PolarsResult<LazyFrame> {}
-// }
+pub trait MongoLazyReader {
+    fn scan_mongo_collection(
+        collection: Collection<BsonDoc>,
+        find_doc: BsonDoc,
+        options: &BsonScanOptions,
+    ) -> PolarsResult<LazyFrame> {
+        let f = BsonScan::new(collection, Some(find_doc), options);
 
-// impl BsonLazyReader for LazyFrame {}
+        let args = ScanArgsAnonymous {
+            name: "MONGO SCAN",
+            infer_schema_length: options.infer_schema_length,
+            n_rows: options.n_rows,
+            ..ScanArgsAnonymous::default()
+        };
+
+        LazyFrame::anonymous_scan(Arc::new(f), args)
+    }
+}
+
+impl MongoLazyReader for LazyFrame {}
